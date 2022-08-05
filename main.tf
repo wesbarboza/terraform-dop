deaeb4da6f3c41298f8a1cd9e49c95344a5f134b
weslley.barboza <weslley_barboza@hst.com.br>
2022-08-04 21:28:12 -0300

    feat: atualizando main.tf

--- main.tf
+++ main.tf
@@ -39,5 +39,5 @@ output "kube_endpoint" {
 
 resource "local_file" "kube_config" {
     content  = digitalocean_kubernetes_cluster.k8s_iniciativa.kube_config.0.raw_config
-    filename = "kube_config.yaml"
+    filename = "kubeconfig.yaml"
 }
